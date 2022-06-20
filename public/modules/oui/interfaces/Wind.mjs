4d110aad9514a89958e4bd03f272b9c9ceb39ab4
Axford <Damian@axford.me.uk>
2022-06-21 00:57:22 +0100

    Add histogram to wind ui

--- public/modules/oui/interfaces/Wind.mjs
+++ public/modules/oui/interfaces/Wind.mjs
@@ -33,14 +33,36 @@ export default class Wind {
     this.channel = channel;
     this.state = state;
     this.built = false;
+    this.receivedWind = false;
 
     this.rawVectors = [];  // history of raw vector values
+
+    this.numBins = 24;
+    this.histoBins = [];
+    // init histogram bins
+    for (var i=0; i<this.numBins; i++) {
+      this.histoBins.push(0);
+    }
+
+    // start sampling timer
+    //var wind = this.state.getParamValues(node, channel, 14, [0])[0];
+    setInterval(()=>{
+      if (!this.receivedWind) return;
+
+      var wind = this.state.getParamValues(this.channel.node.id, this.channel.channel, 14, [0])[0];
+
+      // add to bin
+      var bin = Math.floor((wind / (360 / this.numBins))) % this.numBins;
+      this.histoBins[bin]++;
+    }, 1000)
 	}
 
 	onParamValue(data) {
 
-		// heading
+		// wind
 		if (data.param == 14 && data.msgType == DLM.DRONE_LINK_MSG_TYPE_FLOAT) {
+      this.receivedWind = true;
+
 			// pass onto node for mapping
 			this.channel.node.updateMapParam('wind', 3, data.values, this.channel.channel, 14);
 		}
@@ -80,15 +102,38 @@ export default class Wind {
 		ctx.fillStyle = '#343a40';
 		ctx.fillRect(0,0,w1,h);
 
+    // histogram
+    var maxBin = 1;
+    for (var i=0; i<this.numBins; i++) {
+      if (this.histoBins[i] > maxBin) maxBin = this.histoBins[i];
+    }
+
+    for (var i=0; i<this.numBins; i++) {
+      var a1 = 2 * Math.PI * i / this.numBins - Math.PI/2;
+      var a2 = a1 + 2 * Math.PI / this.numBins;
+      var r = 30 + 50 * this.histoBins[i] / maxBin;
+
+      ctx.fillStyle = '#55f';
+      ctx.beginPath();
+      ctx.arc(cx, 100, r, a1, a2);
+      ctx.lineTo(cx,100);
+      ctx.fill();
+    }
+
+
+    // rings
     ctx.strokeStyle = '#fff';
+    ctx.fillStyle = '#343a40';
     ctx.lineWidth = 1;
     ctx.beginPath();
     ctx.arc(cx, 100, 80, 0, 2 * Math.PI);
     ctx.stroke();
 		ctx.beginPath();
     ctx.arc(cx, 100, 30, 0, 2 * Math.PI);
+    ctx.fill();
     ctx.stroke();
 
+    // ticks
     ctx.beginPath();
     for (var i =0; i<12; i++) {
       var ang = (i*30) * Math.PI / 180;
@@ -105,6 +150,7 @@ export default class Wind {
     ctx.lineTo(cx + 90*Math.cos(wind2), 100 + 90*Math.sin(wind2) );
     ctx.stroke();
 
+    // text
     ctx.fillStyle = '#5F5';
     ctx.font = '20px bold serif';
 		ctx.textAlign = 'center';
