104df9cad43490ef5dc12c05128abe7745240c70
Axford <Damian@axford.me.uk>
2022-12-26 15:41:19 +0000

    Cell capacity estimation

--- public/modules/oui/interfaces/INA3221.mjs
+++ public/modules/oui/interfaces/INA3221.mjs
@@ -3,12 +3,45 @@ import * as DLM from '../../droneLinkMsg.mjs';
 
 //loadStylesheet('./css/modules/interfaces/INA3221.css');
 
+// capacity curve... approximate
+// ref: https://blog.ampow.com/lipo-voltage-chart/
+// voltages for capacities from 0 to 100
+//                                0     10    20    30    40   50    60    70    80    90    100
+const batteryCapacityCurve = [3.27, 3.69, 3.73, 3.77, 3.8, 3.84, 3.87, 3.95, 4.02, 4.11, 4.2];
+
+
 export default class INA3221 {
 	constructor(channel, state) {
     this.channel = channel;
     this.state = state;
     this.built = false;
 	}
+
+  estimateCellCapacity(v) {
+    // find nearest reading in capacity curve
+    var cap = 0;
+
+    // check voltage is above minimum
+    if (v < batteryCapacityCurve[0]) return 0;
+
+    // find which region of the battery curve we're in
+    for (var i=0; i < 11; i++) {
+      if (v >= batteryCapacityCurve[i]) {
+        cap = i;
+      }
+    }
+
+    // lerp between battery levels to calc final percentage
+    if (cap < 10) {
+      // calc fractional compenent
+      var f = ( v - batteryCapacityCurve[cap]) / (batteryCapacityCurve[cap+1] - batteryCapacityCurve[cap]);
+      cap = cap*10 + f*10;
+    } else {
+      cap = 100;
+    }
+    
+    return cap;
+  }
 
   drawLabel(ctx, label, x1,y1,w,h) {
     ctx.strokeStyle = '#343a40';
@@ -20,12 +53,12 @@ export default class INA3221 {
     ctx.fillText(label, x1+w/2, y1+h);
   }
 
-	drawMeter(ctx, v, x1,y1,w,h) {
+	drawMeter(ctx, v, x1,y1,w,h, clr = '#8F8', s=true) {
     ctx.strokeStyle = '#343a40';
     ctx.strokeRect(x1, y1, x1+w, y1+h);
 
-    ctx.fillStyle = '#8F8';
-    ctx.font = '35px serif';
+    ctx.fillStyle = clr;
+    ctx.font = s ? '35px serif' : '16px serif';
     ctx.fillText(v, x1+w/2, y1+h);
   }
 
@@ -44,7 +77,13 @@ export default class INA3221 {
       power: this.state.getParamValues(node, channel, 13, [0,0,0]),
       loadV: this.state.getParamValues(node, channel, 14, [0,0,0]),
 			cellV: this.state.getParamValues(node, channel, 15, [0,0,0]),
-			alarm: this.state.getParamValues(node, channel, 16, [0,0,0])
+			alarm: this.state.getParamValues(node, channel, 16, [0,0,0]),
+      capacity: [0,0,0]
+    }
+
+    // calc capacities
+    for (var i=0; i<3; i++) {
+      ina.capacity[i] = this.estimateCellCapacity(ina.cellV[i]);
     }
 
     // redraw canvas
@@ -71,8 +110,12 @@ export default class INA3221 {
 
     for (var i=0; i<3; i++) {
       var y1 = hm * i;
+      var clr = ina.capacity[i]<20 ? '#f55' : '#8f8';
       // 'Cell V'
-      this.drawMeter(ctx, ina.cellV[i].toFixed(2), 0, y1, mw,hm);
+      this.drawMeter(ctx, ina.cellV[i].toFixed(2), 0, y1, mw,hm, clr);
+
+      // capacity
+      this.drawMeter(ctx, ina.capacity[i].toFixed(0) + '%', 0, y1+20, mw,hm, clr, false);
 
       // V
   		this.drawMeter(ctx, ina.loadV[i].toFixed(2), w, y1, mw,hm);
