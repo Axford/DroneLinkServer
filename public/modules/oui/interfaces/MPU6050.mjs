6d006e01a2b792ae9a727420ac34db86e8fc76c6
Axford <Damian@axford.me.uk>
2023-08-27 01:05:46 +0100

    Adding calibration vis

--- public/modules/oui/interfaces/MPU6050.mjs
+++ public/modules/oui/interfaces/MPU6050.mjs
@@ -18,15 +18,21 @@ export default class MPU6050 extends ModuleInterface {
     var node = this.channel.node.id;
     var channel = this.channel.channel;
 
+  
 		// keep size updated
 		var w = this.ui.width();
-    var h = Math.max(this.ui.height(), 200);
+    var h = Math.max(this.ui.height(), 400) - 20;
 
     this.renderer.setSize( w, h );
     this.camera.updateProjectionMatrix();
     this.camera.aspect = w/h;
 
     var pos = this.state.getParamValues(node, channel, 10, [0,0,0]);
+
+    var rawVector = this.state.getParamValues(node, channel, 15, [0,0,0,0]);
+    var calibX = this.state.getParamValues(node, channel, 16, [0,0,0]);
+    var calibY = this.state.getParamValues(node, channel, 17, [0,0,0]);
+    var calibZ = this.state.getParamValues(node, channel, 18, [0,0,0]);
 
     this.cube.position.x = pos[0];
     this.cube.position.y = pos[1];
@@ -44,7 +50,21 @@ export default class MPU6050 extends ModuleInterface {
     s += 'y: ' + pos[1].toFixed(1) + '<br/>';
     s += 'z: ' + pos[2].toFixed(1) + '<br/>';
     s += 'mag: ' + m.toFixed(1) + '<br/>';
+    s += 'X Calib: ' + ((calibX[2]-calibX[0])/2).toFixed(1) + ', ' + calibX[1].toFixed(1) + '<br/>';
+    s += 'Y Calib: ' + ((calibY[2]-calibY[0])/2).toFixed(1) + ', ' + calibY[1].toFixed(1) + '<br/>';
+    s += 'Z Calib: ' + ((calibZ[2]-calibZ[0])/2).toFixed(1) + ', ' + calibZ[1].toFixed(1) + '<br/>';
     this.uiOverlay.html(s);
+
+
+
+    // visualise calibration sphere
+    this.calibSphere.position.x = calibX[1];
+    this.calibSphere.position.y = calibY[1];
+    this.calibSphere.position.z = calibZ[1];
+
+    this.calibSphere.scale.x = (calibX[2] - calibX[0])/2;
+    this.calibSphere.scale.y = (calibY[2] - calibY[0])/2;
+    this.calibSphere.scale.z = (calibZ[2] - calibZ[0])/2;
   }
 
 
@@ -62,12 +82,34 @@ export default class MPU6050 extends ModuleInterface {
 	build() {
     super.build('MPU6050');
 
+    this.modeSelect = $('<select class="modeSelect"></select>');
+    // add mode options
+    this.modeSelect.append($('<option value="0">Online Calibration</option>'));
+    this.modeSelect.append($('<option value="1">Fixed Calibration</option>'));
+    this.modeSelect.append($('<option value="2">Reset Calibration</option>'));
+    this.modeSelect.append($('<option value="3">Store Calibration</option>'));
+    this.modeSelect.change((e)=>{
+      // get value
+      var newMode = this.modeSelect.val();
+
+      var qm = new DLM.DroneLinkMsg();
+			qm.node = this.channel.node.id;
+			qm.channel = this.channel.channel;
+			qm.param = 19;
+			qm.setUint8([ newMode ]);
+			this.state.send(qm);
+
+      this.queryParam(19);
+    });
+
+    this.ui.append(this.modeSelect);
+
     this.uiOverlay = $('<div style="position:absolute; z-index:1000; padding: 4px 8px; color:white">test</div>');
     this.ui.append(this.uiOverlay);
 
-    var w = Math.max(this.ui.width(), 100);
-    var h = Math.max(this.ui.height(), 200);
-    
+    var w = Math.max(this.ui.width(), 200);
+    var h = Math.max(this.ui.height(), 400);
+
     console.log('THREE', w, h);
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera( 75, w / h, 0.1, 1000 );
@@ -79,7 +121,7 @@ export default class MPU6050 extends ModuleInterface {
 
     const controls = new OrbitControls( this.camera, this.renderer.domElement );
     controls.minDistance = 10;
-    controls.maxDistance = 20;
+    controls.maxDistance = 50;
     controls.maxPolarAngle = Math.PI / 2;
 
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -100,6 +142,24 @@ export default class MPU6050 extends ModuleInterface {
     this.scene.add( new THREE.AxesHelper( 10 ) );
 
     this.camera.position.z = 12;
+
+    this.sphereGeometry = new THREE.SphereGeometry( 0.2, 6, 6 );
+    this.sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+
+    this.sphereMaterial2 = new THREE.MeshBasicMaterial( { color: 0xff00ff } );
+
+    this.calibSphereGeometry = new THREE.SphereGeometry( 1, 20, 20 );  
+    this.calibSphereMaterial = new THREE.MeshDepthMaterial( { wireframe:true, opacity:0.5 } );
+    this.calibSphere = new THREE.Mesh( this.calibSphereGeometry, this.calibSphereMaterial );
+    this.scene.add( this.calibSphere );
+
+    // see if we already know key params
+    var node = this.channel.node.id;
+    var channel = this.channel.channel;
+
+    // mode
+    var mode = this.state.getParamValues(node, channel, 19, [0])[0];
+    this.modeSelect.val(mode);
 
     this.finishBuild();
 
