1dbb5fe9d18fa26ab71b70258d4106b41beb2d1b
Axford <Damian@axford.me.uk>
2023-09-19 22:32:16 +0100

    Wizard progress

--- public/modules/oui/Wizard.mjs
+++ public/modules/oui/Wizard.mjs
@@ -23,23 +23,22 @@ class InputWizard {
     this.str = str; // will be overwritten by non-literals
     this.container = container;
     this.isLiteral = !str.startsWith("{{");
-    this.isComment = false; 
+    this.isComment = false;
     this.data = {};
     this.hasInput = false;
 
     if (this.isLiteral) {
-        // see if its a comment
-        if (str.startsWith(';')) {
-            this.isComment = true;
-        } else {
-            // otherwise parse name = value pair
-            var epos = str.indexOf('=');
-            this.data = {
-                name: str.substring(0,epos).trim(),
-                value: str.substring(epos+1, str.length).trim()
-            }
-        }
-
+      // see if its a comment
+      if (str.startsWith(";")) {
+        this.isComment = true;
+      } else {
+        // otherwise parse name = value pair
+        var epos = str.indexOf("=");
+        this.data = {
+          name: str.substring(0, epos).trim(),
+          value: str.substring(epos + 1, str.length).trim(),
+        };
+      }
     } else {
       // parse JSON object
       var json = str.substring(1, str.length - 1);
@@ -62,6 +61,9 @@ class InputWizard {
     this.updateWidget();
 
     // build widget UI
+
+    this.inputs = [];  // form input elements
+
     this.buildWidgetUI();
   }
 
@@ -84,34 +86,460 @@ class InputWizard {
 
   updateWidget() {
     if (this.isConfigured()) {
-        this.ui.widget.addClass('configured');
+      this.ui.widget.addClass("configured");
     } else {
-        this.ui.widget.removeClass('configured');
+      this.ui.widget.removeClass("configured");
     }
+
+    if (this.str == "") {
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
+
+  show() {
+    this.ui.widget.show();
+  }
+
+  hide() {
+    this.ui.widget.hide();
+  }
+
+  buildCharInput() {
+    var me = this;
+    // generate a form input
+    var ele = $('<input class="form-control" type="text" placeholder="">');
+    if (this.data.default) {
+      ele.val(this.data.default);
+      me.gotInput();
+    }
+    ele.on("change", () => {
+      var v = ele.val();
+      me.str = me.data.param + ' = "' + v + '"';
+      if (v.length > 0) {
+        me.gotInput();
+      }
+    });
+    return ele;
+  }
+
+  buildIntegerInput() {
+    var me = this;
+    // generate a form input
+    var ele = $('<input class="form-control" type="text" placeholder="">');
+    if (this.data.default) {
+      ele.val(this.data.default);
+      me.gotInput();
+    }
+    ele.on("change", () => {
+      var v = parseInt(ele.val());
+      me.str = me.data.param + " = " + v;
+      me.gotInput();
+    });
+    return ele;
+  }
+
+  buildFloatInput() {
+    var me = this;
+    // generate a form input
+    var ele = $('<input class="form-control" type="text" placeholder="">');
+    if (this.data.default) {
+      ele.val(this.data.default);
+      me.gotInput();
+    }
+    ele.on("change", () => {
+      var v = parseFloat(ele.val());
+      me.str = me.data.param + " = " + v.toFixed(4);
+      me.gotInput();
+    });
+    return ele;
+  }
+
+  buildI2CBusInput() {
+    var me = this;
+
+    // container
+    var container = $('<div></div>');
+
+    // generate a form input
+    var ele = $('<select class="form-control" >');
+    var op = $('<option value="">-</option>');
+    ele.append(op);
+    
+    for (var i=0; i<8; i++) {
+        var op = $('<option value="'+i+'">'+ i +'</option>');
+        ele.append(op);
+    };
+
+    if (me.data.default !== undefined) {
+        ele.val(me.data.default);
+        me.gotInput();
+    }
+    
+    ele.on("change", () => {
+      var v = ele.val();
+      me.str = me.data.param + " = " + v;
+      if (v.length > 0) {
+        me.gotInput();
+      }
+    });
+    container.append(ele);
+
+    var info = $('<div class="text-muted mt-2"></div>');
+
+    // get i2c info button
+    var btn = $('<button class="btn btn-primary mt-2">Autodetect</button>');
+    btn.on('click', ()=>{
+        // determine the expected i2c address of this module
+        var addr = parseInt(me.mgr.modInfo.I2CAddress[0], 16);
+        info.html('Looking for address: ' + addr);
+
+        // attempt to fetch latest i2c scan info
+        //console.log(me.mgr.mgr.node);
+        var node = me.mgr.mgr.node;
+        if (!node.ipAddress) {
+            $(btn).notify(
+                "IP address unknown",
+                {
+                  className: "error",
+                  autoHide: true,
+                  arrowShow: false,
+                  position: "bottom",
+                }
+              );
+        }
+        $.getJSON("http://" + node.ipAddress + "/i2c", function (data) {
+            console.log(data);
+            var chan = -1;
+            data.forEach((channel)=>{
+                channel.addresses.forEach((address)=>{
+                    if (address == addr) {
+                        chan = channel.channel;
+                    }
+                });
+            });
+            if (chan > -1) {
+                info.html('Found on bus: ' + chan);
+                ele.val(chan).trigger('change');
+            } else {
+                info.html('Could not find a device with address: ' + addr);
+            }
+        });
+    });
+    container.append(btn);
+    container.append(info);
+
+    return container;
+  }
+
+  buildSerialPortInput() {
+    var me = this;
+    // generate a form input
+    var ele = $('<select class="form-control" >');
+    var op = $('<option value="">-</option>');
+    ele.append(op);
+    
+    for (var i=0; i<3; i++) {
+        var op = $('<option value="'+i+'">'+ i +'</option>');
+        ele.append(op);
+    };
+
+    if (me.data.default !== undefined) {
+        ele.val(me.data.default);
+        me.gotInput();
+    }
+    
+    ele.on("change", () => {
+      var v = ele.val();
+      me.str = me.data.param + " = " + v;
+      if (v.length > 0) {
+        me.gotInput();
+      }
+    });
+    return ele;
+  }
+
+  buildPinsInput() {
+    var me = this;
+
+    // container
+    var container = $('<div></div>');
+
+    // generate a form inputs for each pin, based on length of capabilities array
+
+    var numPins = me.data.capabilities.length;
+
+    for (var i=0; i<numPins; i++) {
+        var ele = $('<select class="form-control" >');
+
+        me.inputs.push(ele);
+
+        var op = $('<option value="">-</option>');
+        ele.append(op);
+        
+        ele.on("change", () => {
+            me.onChange();
+        });
+        container.append(ele);
+    }
+
+    var info = $('<div class="text-muted mt-2"></div>');
+
+    // load pin info
+    var btn = $('<button class="btn btn-primary mt-2">Scan for Available/Capable Pins</button>');
+    btn.on('click', ()=>{
+        // determine the expected i2c address of this module
+        info.html('Loading pin info...');
+
+        // attempt to fetch latest pin info
+        //console.log(me.mgr.mgr.node);
+        var node = me.mgr.mgr.node;
+        if (!node.ipAddress) {
+            $(btn).notify(
+                "IP address unknown",
+                {
+                  className: "error",
+                  autoHide: true,
+                  arrowShow: false,
+                  position: "bottom",
+                }
+              );
+        }
+        $.getJSON("http://" + node.ipAddress + "/pins", function (data) {
+            console.log(data);
+            // add options to each element if they are available and have matching capabilities
+            data.forEach((pin)=>{
+                if (pin.state == 1) {
+                    // compare to each capability
+                    for (var i=0; i<numPins; i++) {
+                        if (pin[me.data.capabilities[i]] == 1) {
+                            me.inputs[i].append( $('<option value="'+pin.id+'">'+pin.id+'</option>') );
+                        }
+                    }
+                }
+            });
+
+            info.html('Pin info loaded');
+        });
+    });
+    container.append(btn);
+    container.append(info);
+
+    return container;
+  }
+
+  onChange() {
+    var me = this;
+
+    var vStr = '';
+
+    if (this.data.type == "char") {
+        vStr = me.inputs[0].val();
+    } else if (this.data.type == "uint8_t" || this.data.type == "uint32_t" || this.data.type == "I2CBus" || this.data.type == "serialPort" || this.data.type == "pins") {
+      me.inputs.forEach((ele, index)=>{
+        if (index > 0) vStr += ', ';
+        vStr += ele.val();
+      });  
+    } else if (this.data.type == "float") {
+        me.inputs.forEach((ele, index)=>{
+            if (index > 0) vStr += ', ';
+            vStr += ele.val().toFixed(4);
+          });  
+    } 
+    me.str = me.data.param + " = " + vStr;
+    if (vStr.length > 0) {
+        me.gotInput();
+    }
+  }
+
+  gotInput() {
+    this.hasInput = true;
+    this.updateWidget();
+    this.mgr.updateWidget();
   }
 
   buildWidgetUI() {
     var me = this;
-    if (this.data.type == 'char') {
-        // generate a form input
-        var container = $('<div class="form-group"><label>'+this.data.title+'</label></div>');
-        var ele = $('<input class="form-control" type="text" placeholder="">');
-        ele.on('change', ()=>{
-            var v = ele.val();
-            me.str = me.data.param + ' = "' + v + '"'; 
-            if (v.length > 0) {
-                me.hasInput = true;
-                me.updateWidget();
-                me.mgr.updateWidget();
-            }
-        });
-        container.append(ele);
-        this.ui.widget.append(container);
-    } else if (this.isLiteral) {
-        this.ui.widget.html(this.str);
+    if (this.isLiteral) {
+      this.ui.widget.html(this.str);
     } else {
+      var container = $(
+        '<div class="form-group"><label>' + this.data.title + "</label></div>"
+      );
+      this.ui.widget.append(container);
+
+      if (this.data.type == "char") {
+        container.append(this.buildCharInput());
+      } else if (this.data.type == "uint8_t" || this.data.type == "uint32_t") {
+        container.append(this.buildIntegerInput());
+      } else if (this.data.type == "float") {
+        container.append(this.buildFloatInput());
+      } else if (this.data.type == "I2CBus") {
+        container.append(this.buildI2CBusInput());
+      } else if (this.data.type == "serialPort") {
+        container.append(this.buildSerialPortInput());
+      } else if (this.data.type == "pins") {
+        container.append(this.buildPinsInput());
+      } else {
         // unknown?
-        this.ui.widget.html('UNKNOWN TYPE: ' + this.str);
+        var ele = $(
+          "<div>" +
+            "UNKNOWN TYPE: " +
+            this.data.type +
+            ": " +
+            this.str +
+            "</div>"
+        );
+        container.append(ele);
+      }
+    }
+  }
+}
+
+/*
+  Manages the input selection for a ModuleType
+*/
+class TypeWizard {
+  constructor(mgr, str, container) {
+    this.mgr = mgr;
+    this.str = str; // will be overwritten by non-literals
+    this.container = container;
+    this.isLiteral = !str.startsWith("{{");
+    this.isComment = false;
+    this.data = {};
+    this.hasInput = false;
+
+    if (this.isLiteral) {
+      // see if its a comment
+      if (str.startsWith(";")) {
+        this.isComment = true;
+      } else {
+        // otherwise parse name = value pair
+        var epos = str.indexOf("=");
+        this.data = {
+          name: str.substring(0, epos).trim(),
+          value: str.substring(epos + 1, str.length).trim(),
+        };
+      }
+    } else {
+      // parse JSON object
+      var json = str.substring(1, str.length - 1);
+      try {
+        this.data = JSON.parse(json);
+      } catch (e) {
+        console.error("Error parsing json data in recipe: " + json);
+      }
+
+      // parse data object and update str
+      this.updateStr();
+    }
+
+    // build UI
+    this.ui = {};
+
+    this.ui.widget = $('<div class="typeWidget"></div>');
+    this.container.append(this.ui.widget);
+
+    this.updateWidget();
+
+    // build widget UI
+    this.buildWidgetUI();
+  }
+
+  updateStr() {
+    
+  }
+
+  isConfigured() {
+    return this.isLiteral || this.hasInput;
+  }
+
+  toConfigString() {
+    return '[ ' + this.str + ' ]';
+  }
+
+  typeName() {
+    // return type name
+    if (!this.isConfigured()) return '';
+    var epos = this.str.indexOf("=");
+    return this.str.substring(0, epos).trim();
+  }
+
+  updateWidget() {
+    if (this.isConfigured()) {
+      this.ui.widget.addClass("configured");
+    } else {
+      this.ui.widget.removeClass("configured");
+    }
+
+    if (this.str == "") {
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
+
+  show() {
+    this.ui.widget.show();
+  }
+
+  hide() {
+    this.ui.widget.hide();
+  }
+
+  buildModuleTypeInput() {
+    var me = this;
+    // generate a form input
+    var ele = $('<select class="form-control" >');
+    var op = $('<option value="">-</option>');
+    ele.append(op);
+    
+    me.data.options.forEach((option)=>{
+        var op = $('<option value="'+option+'">'+ option +'</option>');
+        ele.append(op);
+    });
+    
+    ele.on("change", () => {
+      var v = ele.val();
+      me.str = v + ' = ' + me.data.value;
+      if (v.length > 0) {
+        me.gotInput();
+      }
+    });
+    return ele;
+  }
+
+  gotInput() {
+    this.hasInput = true;
+    this.updateWidget();
+    this.mgr.updateWidget();
+  }
+
+  buildWidgetUI() {
+    var me = this;
+    if (this.isLiteral) {
+      this.ui.widget.html('[ ' + this.str + ' ]');
+    } else {
+      var container = $(
+        '<div class="form-group"></div>'
+      );
+      this.ui.widget.append(container);
+
+      if (this.data.type == "moduleType") {
+        container.append(this.buildModuleTypeInput());
+      } else {
+        // unknown?
+        var ele = $(
+          "<div>" +
+            "UNKNOWN TYPE: " +
+            this.data.type +
+            ": " +
+            this.str +
+            "</div>"
+        );
+        container.append(ele);
+      }
     }
   }
 }
@@ -126,42 +554,55 @@ class ModuleWizard {
     this.leftPane = leftPane;
     this.rightPane = rightPane;
     this.params = [];
-    this.name = '';
+    this.name = "";
     this.selected = false;
 
     // create ui elements
     this.ui = {};
 
-    this.ui.widget = $('<div class="moduleWidget">'+str+'</div>');
-    this.ui.widget.on('click', ()=>{
-        me.mgr.selectModule(me);
+    this.ui.widget = $('<div class="moduleWidget">' + str + "</div>");
+    this.ui.widget.on("click", () => {
+      me.mgr.selectModule(me);
     });
     this.leftPane.append(this.ui.widget);
 
-    // is this the overall node dummy module?
-    if (str == '') {
-        this.ui.widget.html('Node Settings');
-    }
-
     // container for inputs
-    this.ui.inputsContainer = $('<div style="display:none"></div>');
+    this.ui.inputsContainer = $('<div class="moduleInputsContainer" style="display:none"></div>');
     this.rightPane.append(this.ui.inputsContainer);
 
-    this.typeInput = new InputWizard(this, str, this.ui.inputsContainer);
+    this.ui.typeContainer = $('<div class="mb-4"><h2>Module Type</h2></div>');
+    this.ui.inputsContainer.append(this.ui.typeContainer);
+
+    // is this the overall node dummy module?
+    if (str == "") {
+        this.ui.widget.html("Node Settings");
+        this.ui.typeContainer.hide();
+    }
+
+    this.ui.paramsContainer = $('<div></div>');
+    if (str != '') {
+        this.ui.paramsContainer.append($('<h2>Module Configuration</h2>'));
+    }
+    this.ui.inputsContainer.append(this.ui.paramsContainer);
+
+    this.typeInput = new TypeWizard(this, str, this.ui.typeContainer);
   }
 
   parseLine(str) {
-    var newInput = new InputWizard(this, str, this.ui.inputsContainer);
+    var newInput = new InputWizard(this, str, this.ui.paramsContainer);
     // see if this was our name
-    console.log(newInput);
-    if (newInput.isLiteral && !newInput.isComment && newInput.data.name == 'name') {
-        this.name = newInput.data.value;
-        // strip double quotes?
-        if (this.name.startsWith('"')) {
-            this.name = this.name.substring(1,this.name.length-1);
-        }
-        this.ui.widget.html(this.name);
-    } 
+    if (
+      newInput.isLiteral &&
+      !newInput.isComment &&
+      newInput.data.name == "name"
+    ) {
+      this.name = newInput.data.value;
+      // strip double quotes?
+      if (this.name.startsWith('"')) {
+        this.name = this.name.substring(1, this.name.length - 1);
+      }
+      this.ui.widget.html(this.name);
+    }
     this.params.push(newInput);
   }
 
@@ -170,7 +611,7 @@ class ModuleWizard {
 
     // module section
     if (this.typeInput.str != "") {
-      res += "[ " + this.typeInput.toConfigString() + " ]\n";
+      res += this.typeInput.toConfigString() + "\n";
     }
 
     // params
@@ -194,23 +635,39 @@ class ModuleWizard {
   }
 
   updateWidget() {
+    var me = this;
     if (this.isConfigured()) {
-        this.ui.widget.addClass('configured');
+      this.ui.widget.addClass("configured");
     } else {
-        this.ui.widget.removeClass('configured');
+      this.ui.widget.removeClass("configured");
     }
+
+    // attempt to load module info if type is defined
+    if (me.typeInput.isConfigured()) {
+        this.modInfo = moduleInfo[me.typeInput.typeName()]; 
+    }
+
+    // show or hide input widgets based on typeInput
+    this.params.forEach((p) => {
+      if (me.typeInput.isConfigured()) {
+        p.show();
+      } else {
+        p.hide();
+      }
+    });
+
   }
 
   select() {
     this.ui.inputsContainer.show();
     this.selected = true;
-    this.ui.widget.addClass('selected');
+    this.ui.widget.addClass("selected");
   }
 
   blur() {
     this.ui.inputsContainer.hide();
     this.selected = false;
-    this.ui.widget.removeClass('selected');
+    this.ui.widget.removeClass("selected");
   }
 }
 
@@ -313,11 +770,11 @@ export default class Wizard {
   selectModule(module) {
     console.log(module);
     this.modules.forEach((m) => {
-        if (m == module) {
-            m.select();
-        } else {
-            m.blur();
-        }
+      if (m == module) {
+        m.select();
+      } else {
+        m.blur();
+      }
     });
   }
 
@@ -377,7 +834,12 @@ export default class Wizard {
     this.modules = [];
 
     // setup syntax parser
-    var module = new ModuleWizard(this, "", this.ui.leftPane, this.ui.rightPane);
+    var module = new ModuleWizard(
+      this,
+      "",
+      this.ui.leftPane,
+      this.ui.rightPane
+    );
     this.modules.push(module);
 
     // parse rawRecipe into lines
@@ -392,7 +854,12 @@ export default class Wizard {
           // check ends with a ]
           if (line[line.length - 1] == "]") {
             // instance a new module
-            var module = new ModuleWizard(this, line.substring(1, line.length - 1), this.ui.leftPane, this.ui.rightPane);
+            var module = new ModuleWizard(
+              this,
+              line.substring(1, line.length - 1),
+              this.ui.leftPane,
+              this.ui.rightPane
+            );
             this.modules.push(module);
           } else {
             // error, no closing bracket
@@ -408,8 +875,8 @@ export default class Wizard {
 
     // update widgets now config fully loaded
     this.modules.forEach((m) => {
-        m.updateWidget();
-      });
+      m.updateWidget();
+    });
   }
 
   generateConfig() {
